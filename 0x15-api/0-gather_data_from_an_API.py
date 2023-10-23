ee76259c4a30baa13a6667f2ce4171a651e2a7fe
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-23 12:32:02 +0000

    new updates

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script to make a request from an API using test from JSONPlaceholder"""
+"""Script to make a request from an API using test JSONPlaceholder"""
 
 import json
 import requests
@@ -7,20 +7,20 @@ import sys
 ''' Importing the necessary libraries needed for this project'''
 
 if __name__ == "__main__":
-  url = 'https://jsonplaceholder.typicode.com/'
-  user = '{}users/{}'.format(url, sys.argv[1])
-  response = requests.get(user)
-  res_json = response.json()
-  print('Employee {} is done with tasks'.format(res_json.get('name'),end=""))
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = '{}users/{}'.format(url, sys.argv[1])
+    response = requests.get(user)
+    res_json = response.json()
+    print('Employee {} is done with tasks'.format(res_json.get('name'),end=""))
 
-  to_dolist = '{}todos?userId={}'.format(url, sys.argv[1])
-  res = requests.get(to_dolist)
-  resj = res.json()
-  task = []
-  for x in resj:
-     if x.get('completed') is True:
-       task.append(x)
+    to_dolist = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(to_dolist)
+    resj = res.json()
+    task = []
+    for x in resj:
+        if x.get('completed') is True:
+            task.append(x)
 
-  print('({}/{}):'.format(len(task), len(resj)))
-  for y in task:
-     print("\t {}".format(y.get("title")))
+    print('({}/{}):'.format(len(task), len(resj)))
+    for y in task:
+        print("\t {}".format(y.get("title")))
