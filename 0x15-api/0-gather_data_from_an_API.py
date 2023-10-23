c5c4c46aa07ed89779a0c5bb8ca02e8d096587d7
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-23 12:18:58 +0000

    new updates

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,20 +6,21 @@ import requests
 import sys
 ''' Importing the necessary libraries needed for this project'''
 
-url = 'https://jsonplaceholder.typicode.com/'
-users = '{}users/{}'.format(url, sys.argv[1])
-response = requests.get(users)
-res_json = response.json()
-print('Employee {} is done with tasks'.format(res_json.get('name'),end=' '))
+if __name__ == "__main__":
+  url = 'https://jsonplaceholder.typicode.com/'
+  users = '{}users/{}'.format(url, sys.argv[1])
+  response = requests.get(users)
+  res_json = response.json()
+  print('Employee {} is done with tasks'.format(res_json.get('name'),end=' '))
 
-to_dolist = '{}todos?userId={}'.format(url, sys.argv[1])
-res = requests.get(to_dolist)
-resj = res.json()
-task = []
-for x in resj:
-    if x.get('completed') is True:
-      task.append(x)
+  to_dolist = '{}todos?userId={}'.format(url, sys.argv[1])
+  res = requests.get(to_dolist)
+  resj = res.json()
+  task = []
+  for x in resj:
+     if x.get('completed') is True:
+       task.append(x)
 
-print('{}/{}'.format(len(task), len(resj)))
-for y in task:
- print("\n {}".format(y.get("title")))
+  print('{}/{}'.format(len(task), len(resj)))
+  for y in task:
+     print("\n {}".format(y.get("title")))
