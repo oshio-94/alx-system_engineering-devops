565b923a8ea6385f1f3634650f68f9afab7a7472
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-23 15:30:27 +0000

    new updates

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Script to make a request from an API using test JSONPlaceholder"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = '{}users/{}'.format(url, sys.argv[1])
+    response = requests.get(user)
+    res_jsn = response.json()
+    username = res_jsn.get('username')
+
+    to_dolist = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(to_dolist)
+    resj = res.json()
+    task = []
+    for x in resj:
+        task.extend([sys.argv[1], username, x.get('completed'), x.get('title')])
+
+    print(task)
