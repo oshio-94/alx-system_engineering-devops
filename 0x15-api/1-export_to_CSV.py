dab72b9424a242ebaf658d24e529b5b1e7ab820f
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-23 19:00:29 +0000

    new updates

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,16 +12,17 @@ if __name__ == "__main__":
     user = '{}users/{}'.format(url, sys.argv[1])
     response = requests.get(user)
     res_jsn = response.json()
-    username = res_jsn.get('username')
+    usrname = res_jsn.get('username')
 
     to_dolist = '{}todos?userId={}'.format(url, sys.argv[1])
     res = requests.get(to_dolist)
     resj = res.json()
     task = []
     for x in resj:
-        task.extend([sys.argv[1], username, x.get('completed'), x.get('title')])
+        task.extend([sys.argv[1], usrname, x.get('completed'), x.get('title')])
 
     filename = '{}.csv'.format(sys.argv[1])
     with open(filename, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', quotechar="'", quoting=csv.QUOTE_ALL)
+        csvwriter = csv.writer(csvfile, 
+                delimiter=',', quotechar="'", quoting=csv.QUOTE_ALL)
         csvwriter.writerow(task)
