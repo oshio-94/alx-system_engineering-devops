8c546e96309e2c48906e94cdd61a3442a15f02c2
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-24 09:38:02 +0000

    new updates

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
                         "completed": x.get('completed')}
             task.append(dct_task)
 
-        full_task = {str(userid): task}
+    full_task = {str(userid): task}
     filename = 'todo_all_employees.json'
     with open(filename, 'w') as fname:
         json.dump(full_task, fname)
