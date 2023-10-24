ae189617bb5525e8db0757aaa68c3ea1d07877a4
John Ikhanoba <oshioi@yahoo.co.uk>
2023-10-24 08:59:18 +0000

    new updates

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Script that uses JSONPlaceholder API to get information about employee """
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = '{}users'.format(url)
+    res = requests.get(user)
+    response = res.json()
+    dict_task = {}
+    for user in response:
+        name = user.get('username')
+        userid = user.get('id')
+        todos = '{}todos?userId={}'.format(url, userid)
+        res = requests.get(todos)
+        tasks = res.json()
+        task = []
+        for x in tasks:
+            dct_task = {"username": name,
+                         "task": x.get('title'),
+                         "completed": x.get('completed')}
+            task.append(dct_task)
+
+    full_task = {str(userid): task}
+    filename = 'todo_all_employees.json'
+    with open(filename, 'w') as fname:
+        json.dump(full_task, fname)
+
