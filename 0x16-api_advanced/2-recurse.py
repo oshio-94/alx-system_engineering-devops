f4bf2e994206aaa4e3de3ed07db9e9a50c0b9dba
John Ikhanoba <oshioi@yahoo.co.uk>
2023-11-07 14:22:30 +0000

    updates

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,9 +18,9 @@ def add_title(hot_list, hot_posts):
 
 def recurse(subreddit, hot_list=[], after=None):
     """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
+    version = 'Mozilla/5.0'
     headers = {
-        'User-Agent': u_agent
+        'User-Agent': version
     }
 
     params = {
@@ -28,15 +28,15 @@ def recurse(subreddit, hot_list=[], after=None):
     }
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
+    resp = requests.get(url,
                        headers=headers,
                        params=params,
                        allow_redirects=False)
 
-    if res.status_code != 200:
+    if resp.status_code != 200:
         return None
 
-    dic = res.json()
+    dic = resp.json()
     hot_posts = dic['data']['children']
     add_title(hot_list, hot_posts)
     after = dic['data']['after']
